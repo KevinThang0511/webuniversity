c68a9f84ef2bd20bfa893374ade495608d0b159f
KevinThang0511 <120524177+KevinThang0511@users.noreply.github.com>
2023-06-12 09:24:48 +0700

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,68 @@
+    /* khai báo số 
+let number1 = 35; 
+var number2 = 30;
+    */
+
+    /* khai báo chuỗi 
+var test = ' this is a test'; 
+    */
+
+    /* khai báo mảng 
+var color = ['red','blue','red'];
+    number = [12,23,4,32,4,5]
+        /* thêm vào mảng 
+    color.push('yellow');
+    */
+
+    /* đưa ra màn hình */
+/*
+alert(number1);
+alert(test);
+alert(color[3]);
+alert(color.length); */ /*đưa ra độ dài mảng*/
+
+    /* đưa ra màn hình trong console 
+console.log(12323);
+    */
+
+    /* vòng lăp for 
+for ( var i=0; i<=20; i++) {
+    console.log(i);
+}   
+    */
+
+    /* vòng lặp white 
+    var i=1;
+    while( i <20 ){
+    console.log(i)
+    i++;
+} ;
+    */
+
+    /* câu lệnh if 
+var number1 = 40 ;
+var number2 = 30 ;
+    if (number1 == number2) {
+        console.log('this is true ') ;
+    }  else{
+        console.log('this is false')
+}
+    */
+
+    /* Constructor hàm đối tượng
+function Fruit (name,shape, color){
+    this.name = name ;
+    this.color = color ;
+    this.shape = shape ;
+    this.describe = function(){
+        return 'A ' + this.name + ' is the color ' +
+    this.color ;
+    }
+}
+    var apple = new Fruit ('apple','red','round');
+    console.log(apple.color);
+    console.log(apple.shape);
+    console.log(apple.describe());
+    */
+alert ('hello')
+    
